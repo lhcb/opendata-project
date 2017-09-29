4bd96d9b4ab9417b316695f753a979e1fccf6505
Christopher Burr <christopher.burr@cern.ch>
2017-09-29 17:34:32 +0100

    Make everything more robust

--- Data/setup_main_analysis.py
+++ Data/setup_main_analysis.py
@@ -3,31 +3,56 @@ from __future__ import print_function
 from __future__ import division
 
 import functools
+import hashlib
+import os
 import warnings
-warnings.filterwarnings("ignore")
 from subprocess import check_output
 
 import numpy
 from ipywidgets import interact
 import pandas
-
-rcParams['image.cmap'] = 'Blues'  # change default colormap
-
-pandas.set_option('display.max_columns', None)
-
-# TODO Prevent this happening twice
-_hist = pandas.Series.hist
-@functools.wraps(_hist)
-def new_hist(self, *args, **kwargs):
-    kwargs['histtype'] = 'step'
-    kwargs['grid'] = False
-    return _hist(self, *args, **kwargs)
-pandas.Series.hist = new_hist
-
 from root_pandas import read_root
-
 from scipy import stats as st
 from matplotlib import pyplot as plt
+
+
+warnings.filterwarnings("ignore")
+rcParams['image.cmap'] = 'Blues'  # change default colormap
+pandas.set_option('display.max_columns', None)
+
+
+def check_hash(filename, fn_hash, block_size=65536):
+    if not os.path.isfile(filename):
+        return False
+
+    hasher = hashlib.sha256()
+    with open(filename, 'rb') as afile:
+        buf = afile.read(block_size)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(block_size)
+
+    if hasher.hexdigest() == fn_hash:
+        return True
+    else:
+        print(filename, 'did not match expected hash, retrying')
+        os.remove(filename)
+        return False
+
+
+try:
+    # Ensure this doesn't run twice
+    new_hist
+except NameError:
+    _hist = pandas.Series.hist
+
+    @functools.wraps(_hist)
+    def new_hist(self, *args, **kwargs):
+        kwargs['histtype'] = 'step'
+        kwargs['grid'] = False
+        return _hist(self, *args, **kwargs)
+
+    pandas.Series.hist = new_hist
 
 
 def get_plot_func(data):
@@ -38,5 +63,15 @@ def get_plot_func(data):
     return plot_hist
 
 
-check_output('xrdcp xroot://eospublic.cern.ch//eos/opendata/lhcb/AntimatterMatters2017/data/B2HHH_MagnetDown.root .')
-check_output('xrdcp xroot://eospublic.cern.ch//eos/opendata/lhcb/AntimatterMatters2017/data/B2HHH_MagnetUp.root .')
+eos_server = 'root://eospublic.cern.ch/'
+data_dir = '/eos/opendata/lhcb/AntimatterMatters2017/data/'
+filenames = {
+    'B2HHH_MagnetDown.root': 'b98651b24f825979053544c37010cf7ef9ce5c56ee62357c7e4ae2c392068379',
+    'B2HHH_MagnetUp.root': 'c42ad9e47931e1404bf94ad82ea22a0acd10bc9cfbb58e77a6b0fff08ead7859',
+}
+
+for fn, fn_hash in filenames.items():
+    while not check_hash('Data/'+fn, fn_hash):
+        fn_url = eos_server + data_dir + fn
+        print('Downloading', fn_url)
+        check_output('xrdcp ' + fn_url + ' ./Data/', shell=True)
